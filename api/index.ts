ef8bad7c537f95faf09d38cfca144af10038c283
niveyrojulian <niveyrojulian5@gmail.com>
2025-10-03 19:41:33 -0300

    fix: confirmation

--- api/index.ts
+++ api/index.ts
@@ -10,11 +10,8 @@ export const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  //const token = localStorage.getItem("access_token");
-  //if (token) {
 
   config.headers.Authorization = `Bearer ${process.env.NEXT_COMPANY_TOKEN}`;
-  //}
 
   if (config.data instanceof FormData) {
     delete config.headers["Content-Type"];
